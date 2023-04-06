aebde367452f4408d5f74d7e3534e9f908fc12b0
Alkiro-RA <alkiewiczroger@gmail.com>
2023-04-06 15:18:46 +0200

    2nd commit (master) Open VS Code window at project's path Update path variable's name to describe better what it needs

--- PrepareToCode.ps1
+++ PrepareToCode.ps1
@@ -1,12 +1,13 @@
 # Run VS Code in new window
+$projectPath = "E:\MyPHP\my-backend\php-exceptions\app\public"
 Write-Host "Starting VS Code..."
-Start-Process "code" -ArgumentList "-n"
+Start-Process "code" -ArgumentList "-n", $projectPath
 Write-Host "VS Code is running"
 
 # Run Git Bash at repository's path
-$location = "E:\MyPHP\my-backend"
-Write-Host "Starting Git Bash at '$location'..."
-Start-Process "E:\Programy\Git\bin\bash.exe" -WorkingDirectory $location
+$repoPath = "E:\MyPHP\my-backend"
+Write-Host "Starting Git Bash at '$repoPath'..."
+Start-Process "E:\Programy\Git\bin\bash.exe" -WorkingDirectory $repoPath
 Write-Host "Git Bash is running"
 
 # Run Docker
@@ -15,13 +16,13 @@ Invoke-Item "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 # Wait for the Docker daemon to be ready
 Write-Host "Waiting for Docker to start up."
-Start-Sleep -Seconds 20
+Start-Sleep -Seconds 25
 
 # Set path to container's directory
-$location = "E:\MyPHP\my-backend\php-oop-zoo"
+$containerPath = "E:\MyPHP\my-backend\php-exceptions"
 
 # Run cmd.exe
-Start-Process -FilePath "cmd.exe" -WorkingDirectory $location
+Start-Process -FilePath "cmd.exe" -WorkingDirectory $containerPath
 
 # Run 'docker-compose up" from cmd.exe
 $wshell = New-Object -ComObject wscript.shell
